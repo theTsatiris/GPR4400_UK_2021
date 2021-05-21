d53761405a400ccdeac405b417dba4d678f3f26e
MartinFlannery <72073528+MartinFlannery@users.noreply.github.com>
2021-05-21 15:58:30 +0100

    Update Program.cs

--- VectorProject/VectorProject/Program.cs
+++ VectorProject/VectorProject/Program.cs
@@ -4,24 +4,35 @@ namespace Session2
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            Vector3D aVector = new Vector3D(2, -3, 4);
-            Vector3D bVector = new Vector3D(4, 3, 2);
+            
+            Vector3D aVector = new Vector3D(2, 3, 4);
+            Vector3D bVector = new Vector3D(6, 3, 2);
+            
+            Vector3D vectorSum = aVector.Add(bVector);
+            Console.WriteLine("Vector Sum: " + vectorSum.x + " " + vectorSum.y + " " + vectorSum.z);
 
-            Vector3D sum = aVector.Add(bVector);
-            Console.WriteLine(sum.x + " " + sum.y + " " + sum.z);
-
-            Vector3D sum2 = aVector.Add(3);
-            Console.WriteLine(sum2.x + " " + sum2.y + " " + sum2.z);
+            Vector3D scalarSum = aVector.Add(3);
+            Console.WriteLine("Scalar Sum: " + scalarSum.x + " " + scalarSum.y + " " + scalarSum.z);
 
             Vector3D aScaled = aVector.Scale(2.0f);
-            Console.WriteLine(aScaled.x + " " + aScaled.y + " " + aScaled.z);
+            Console.WriteLine("Scaled Vector: " + aScaled.x + " " + aScaled.y + " " + aScaled.z);
 
             Vector3D aMirrored = aVector.Negate();
-            Console.WriteLine(aMirrored.x + " " + aMirrored.y + " " + aMirrored.z);
-        }
+            Console.WriteLine("Mirrored Vector: " + aMirrored.x + " " + aMirrored.y + " " + aMirrored.z);
+                        
+            for (float delta = 0.0f; delta <= 1.0f; delta += 0.1f)
+            {
+                Console.WriteLine("We are at point " + aVector.LERP(bVector, delta));
+               
+            }
+            
+            aVector.Normalize();
+            bVector.Normalize();
 
+            Console.WriteLine("Dot Product: " + aVector.Dot(bVector));
+            Console.WriteLine("Cosine: " + aVector.Dot(bVector) / (aVector.Magnitude() * (bVector.Magnitude())));
+        }
     }
 }
