efe1bf5a4ad859274dd67778415b90415a64064f
JamesRichardBradley <72073433+JamesRichardBradley@users.noreply.github.com>
2021-05-21 13:36:41 +0100

    Update Vector3D.cs

--- VectorProject/VectorProject/Vector3D.cs
+++ VectorProject/VectorProject/Vector3D.cs
@@ -1,10 +1,69 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace VectorProject
+namespace Vector_Task_1
 {
     class Vector3D
     {
+        private float x, y, z;
+
+        public Vector3D(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector3D()
+        {
+            this.x = 0.0f;
+            this.y = 0.0f;
+            this.z = 0.0f;
+        }
+
+        public Vector3D Add(float value)
+        {
+            Vector3D result = new Vector3D();
+
+            result.x = this.x + value;
+            result.x = this.y + value;
+            result.x = this.z + value;
+
+            return result;
+        }
+
+        public Vector3D Subtract(float value)
+        {
+            Vector3D result = new Vector3D();
+
+            result.x = this.x - value;
+            result.x = this.y - value;
+            result.x = this.z - value;
+
+            return result;
+        }
+
+        public Vector3D Scale(float value)
+        {
+            Vector3D result = new Vector3D();
+
+            result.x = this.x * value;
+            result.x = this.y * value;
+            result.x = this.z * value;
+
+            return result;
+        }
+
+        public Vector3D Negate()
+        {
+            Vector3D result = new Vector3D();
+
+            result.x = -this.x;
+            result.x = -this.y;
+            result.x = -this.z;
+
+            return result;
+        }
     }
 }
