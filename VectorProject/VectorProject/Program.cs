07a0e7fcac56d0bc6db778bc553172b0dcb20a6f
ChrisKuan-SAE <72073451+ChrisKuan-SAE@users.noreply.github.com>
2021-05-21 20:04:18 +0800

    Update Program.cs

--- VectorProject/VectorProject/Program.cs
+++ VectorProject/VectorProject/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+using System;
 
-namespace VectorProject
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vector3D aVector = new Vector3D(2, 1, 4);
+            Vector3D bVector = new Vector3D(5, 2, 6);
+            float scalar = 3;
+
+            Vector3D result = aVector.Add(bVector);
+
+            Console.WriteLine(result);
+
+            Vector3D result2 = aVector.Add(scalar);
+
+            Console.WriteLine(result2);
+
+            Vector3D resultScale = aVector.Scale(2.0f);
+
+            Console.WriteLine(resultScale);
+
+            Vector3D resultNegate = aVector.Negate();
+
+            Console.WriteLine(resultNegate);
         }
     }
 }
