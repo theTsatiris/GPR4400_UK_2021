461392bc81118fd9b6abe565b623d59e430fedad
ChrisKuan-SAE <72073451+ChrisKuan-SAE@users.noreply.github.com>
2021-05-21 22:58:31 +0800

    Update Program.cs

--- VectorProject/VectorProject/Program.cs
+++ VectorProject/VectorProject/Program.cs
@@ -8,23 +8,31 @@ namespace ConsoleApp1
         {
             Vector3D aVector = new Vector3D(2, 1, 4);
             Vector3D bVector = new Vector3D(5, 2, 6);
-            float scalar = 3;
+            //float scalar = 3;
 
-            Vector3D result = aVector.Add(bVector);
+            //Vector3D result = aVector.Add(bVector);
 
-            Console.WriteLine(result);
+            //Console.WriteLine(result);
 
-            Vector3D result2 = aVector.Add(scalar);
+            //Vector3D result2 = aVector.Add(scalar);
 
-            Console.WriteLine(result2);
+            //Console.WriteLine(result2);
 
-            Vector3D resultScale = aVector.Scale(2.0f);
+            //Vector3D resultScale = aVector.Scale(2.0f);
 
-            Console.WriteLine(resultScale);
+            //Console.WriteLine(resultScale);
 
-            Vector3D resultNegate = aVector.Negate();
+            //Vector3D resultNegate = aVector.Negate();
 
-            Console.WriteLine(resultNegate);
+            //Console.WriteLine(resultNegate);
+
+            //for (float delta = 0.0f; delta <= 1.0f; delta += 0.1f)
+            //{
+            //    Console.WriteLine(aVector.Lerp(bVector, delta));
+            //}
+
+            Console.WriteLine("Dot: " + aVector.Dot(bVector));
+            Console.WriteLine("Cosine: " + aVector.Dot(bVector) / (aVector.Magnitude() * bVector.Magnitude()));
         }
     }
 }
