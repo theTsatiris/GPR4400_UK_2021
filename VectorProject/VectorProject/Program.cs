e356f3a8a5eb9e9a53c28b4820496b4862aebc92
JamesRichardBradley <72073433+JamesRichardBradley@users.noreply.github.com>
2021-05-21 13:36:17 +0100

    Update Program.cs

--- VectorProject/VectorProject/Program.cs
+++ VectorProject/VectorProject/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+using System;
 
-namespace VectorProject
+namespace Vector_Task_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vector3D aVector = new Vector3D(2, 6, 12);
+            Vector3D bVector = new Vector3D(3, 5, 7);
+            float value = 2;
+
+            Vector3D resultAdd = aVector.Add(value);
+            Console.WriteLine("Vector3D Values: " + resultAdd);
+
+            Vector3D resultSubtract = aVector.Subtract(value);
+            Console.WriteLine("Vector3D Values: " + resultSubtract);
+
+            Vector3D resultScale = aVector.Scale(value);
+            Console.WriteLine("Vector3D Values: " + resultScale);
+
+            Vector3D resultNegate = aVector.Negate();
+            Console.WriteLine("Vector3D Values: " + resultNegate);
         }
     }
 }
